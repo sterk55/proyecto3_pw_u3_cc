e8f7cdaabb3aeca0c79376cb9cf50fed23ea96d6
sterk55 <Daniel49571@gmail.com>
2023-02-01 19:45:49 -0500

    Taller25

---
+++ src/main/java/com/uce/edu/pw/proyecto3_pw_u3_ec/Proyecto3PwU3EcApplication.java
@@ -0,0 +1,13 @@
+package com.uce.edu.pw.proyecto3_pw_u3_ec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Proyecto3PwU3EcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Proyecto3PwU3EcApplication.class, args);
+	}
+
+}
